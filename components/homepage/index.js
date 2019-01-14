07e65db4017c7b24ff05ffedde98a529baaf33a7
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-01-14 14:51:21 +0700

    [ADD] Homepage

---
+++ components/homepage/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class Homepage extends React.Component{
+    render(){
+        return(
+            <h1>Hello World</h1>
+        )
+    }
+}
+export default Homepage
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,19 @@
-export default () => <div>Welcome to IT3K
-    <h1>Hello</h1>
-</div>
+import React from 'react'
+import HomePage from '../components/homepage'
+// import styled from 'styled-components'
+
+// const Bg = styled.div`
+//   background-color:#E9B6B7;
+//   height: 100vh;
+// `
+class componentName extends React.Component {
+  render () {
+    return (
+      <div>
+        <HomePage />
+      </div>
+    )
+  }
+}
+
+export default componentName;
