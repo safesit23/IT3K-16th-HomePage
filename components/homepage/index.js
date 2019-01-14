f4bd2d9a3494e0d9726f0a88db9eecc1efe24020
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-01-14 15:18:47 +0700

    [UPDATE] edit homepage

--- components/homepage/index.js
+++ components/homepage/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
+// import styled from 'styled-components'
+import {Button} from 'reactstrap'
 
 class Homepage extends React.Component{
     render(){
         return(
+            <div>
             <h1>Hello World</h1>
+            </div>
         )
     }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,6 @@
 import React from 'react'
 import HomePage from '../components/homepage'
-// import styled from 'styled-components'
 
-// const Bg = styled.div`
-//   background-color:#E9B6B7;
-//   height: 100vh;
-// `
 class componentName extends React.Component {
   render () {
     return (
