a7943ea0ac1d086ffdaa9c2d22cec5b6caab8dd8
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-01-21 01:33:15 +0700

    [UPDATE] config project and add styled-comp

---
+++ next.config.js
@@ -0,0 +1,2 @@
+const withCSS = require('@zeit/next-css')
+const withPlugins = require('next-compose-plugins')
--- pages/_document.js
+++ pages/_document.js
@@ -1,25 +1,37 @@
 import Document, { Head, Main, NextScript } from 'next/document'
-// import { ServerStyleSheet } from 'styled-components'
+import { ServerStyleSheet } from 'styled-components'
+
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet()
+
+    const originalRenderPage = ctx.renderPage
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+      })
+
     const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    return {
+      ...initialProps,
+      styles: [...initialProps.styles, ...sheet.getStyleElement()]
+    }
   }
 
   render() {
     return (
       <html>
         <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" />
           <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-          <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
           <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.12.3/antd.min.css" />
+          <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet"></link>
           <script src="https://as.alipayobjects.com/g/component/??es6-shim/0.35.1/es6-sham.min.js,es6-shim/0.35.1/es6-shim.min.js"></script>
         </Head>
-        <body className="custom_class">
-          <Main />
+        <body>
           <NextScript />
+          <Main />
         </body>
       </html>
     )
