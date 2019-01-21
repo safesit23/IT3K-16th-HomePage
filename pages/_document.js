8c8674ac851c4e36bc0bfa028bf5e500cefbc365
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-01-21 10:30:52 +0700

    [UPDATE] eslint

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,8 @@
-import Document, { Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, { Head, Main, NextScript } from "next/document"
+import { ServerStyleSheet } from "styled-components"
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps (ctx) {
     const sheet = new ServerStyleSheet()
 
     const originalRenderPage = ctx.renderPage
@@ -18,16 +18,16 @@ export default class MyDocument extends Document {
     }
   }
 
-  render() {
+  render () {
     return (
       <html>
         <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" />
-          <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-          <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.12.3/antd.min.css" />
-          <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet"></link>
-          <script src="https://as.alipayobjects.com/g/component/??es6-shim/0.35.1/es6-sham.min.js,es6-shim/0.35.1/es6-shim.min.js"></script>
+          <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
+          <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css' />
+          <link rel='stylesheet' type='text/css' charset='UTF-8' href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css' />
+          <link rel='stylesheet' type='text/css' href='https://cdnjs.cloudflare.com/ajax/libs/antd/3.12.3/antd.min.css' />
+          <link href='https://fonts.googleapis.com/css?family=Kanit' rel='stylesheet' />
+          <script src='https://as.alipayobjects.com/g/component/??es6-shim/0.35.1/es6-sham.min.js,es6-shim/0.35.1/es6-shim.min.js' />
         </Head>
         <body>
           <NextScript />
