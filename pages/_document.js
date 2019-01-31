c01362f0fa0931d021ce2865c7c2bb9f7a9c4202
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-01-31 18:24:22 +0700

    [UPDATE] add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -22,7 +22,8 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
+          <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' />
+          <link rel='icon' href='/static/img/logoIT3K-Loading.png' sizes='32x32' />
           <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css' />
           <link rel='stylesheet' type='text/css' charset='UTF-8' href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css' />
           <link rel='stylesheet' type='text/css' href='https://cdnjs.cloudflare.com/ajax/libs/antd/3.12.3/antd.min.css' />
